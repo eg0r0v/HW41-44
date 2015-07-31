4eb0ff937badfe9258243c8eecd5a419b9312ee4
Илья Егоров <Shepherd_MacBook@Ilya.local>
2015-08-01 02:53:16 +0300

    Initial Commit

---
+++ HW41-44/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HW41-44
+//
+//  Created by Илья Егоров on 01.08.15.
+//  Copyright (c) 2015 Илья Егоров. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HW41-44/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HW41-44
+//
+//  Created by Илья Егоров on 01.08.15.
+//  Copyright (c) 2015 Илья Егоров. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
